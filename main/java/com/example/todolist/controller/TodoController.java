8f099becc9f5f6bacb9c3208bca20e5d11d7f580
chaewon9999 <w0729@naver.com>
2025-03-24 13:00:26 +0900

    make a category to start project

---
+++ main/java/com/example/todolist/controller/TodoController.java
@@ -0,0 +1,7 @@
+package com.example.todolist.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+}
---
+++ main/java/com/example/todolist/service/TodoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.todolist.service;
+
+public class TodoServiceImpl implements TodoService{
+}
