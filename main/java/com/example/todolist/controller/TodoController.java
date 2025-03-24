d10ec72877fee0e862b1da9b313fc1348c47a96a
chaewon9999 <w0729@naver.com>
2025-03-24 15:10:17 +0900

    set Basic Controller

--- main/java/com/example/todolist/controller/TodoController.java
+++ main/java/com/example/todolist/controller/TodoController.java
@@ -1,7 +1,33 @@
 package com.example.todolist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/todoLists")
 public class TodoController {
+
+    @PostMapping
+    public void createTodoList() {
+
+    }
+
+    @GetMapping("{/id}")
+    public void getTodoListById() {
+
+    }
+
+    @GetMapping
+    public void getTodoList() {
+
+    }
+
+    @PutMapping
+    public void updateTodoList() {
+
+    }
+
+    @DeleteMapping
+    public void deleteTodoList() {
+
+    }
 }
