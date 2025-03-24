68bf9c7f678a3f207a64c6bdc9f645ccfe0367aa
chaewon9999 <w0729@naver.com>
2025-03-24 13:14:45 +0900

    make entity

--- main/java/com/example/todolist/entity/TodoList.java
+++ main/java/com/example/todolist/entity/TodoList.java
@@ -1,4 +1,29 @@
 package com.example.todolist.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class TodoList {
+
+    private Long id;
+    private String name;
+    private String password;
+    private String contents;
+    private String date;
+
+    public TodoList(String name, String contents, String date) {
+        this.name = name;
+        this.contents = contents;
+        this.date = date;
+    }
+
+    public void updateContents(String contents) {
+        this.contents = contents;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
 }
