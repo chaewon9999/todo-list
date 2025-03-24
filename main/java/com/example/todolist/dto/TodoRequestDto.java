54b449834d721e78979ed493bc15e70ae357268e
chaewon9999 <w0729@naver.com>
2025-03-24 13:33:59 +0900

    create DTO

--- main/java/com/example/todolist/dto/TodoRequestDto.java
+++ main/java/com/example/todolist/dto/TodoRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.todolist.dto;
 
+import lombok.Getter;
+
+@Getter
 public class TodoRequestDto {
+
+    private String name;
+    private String contents;
+    private String password;
+    private String date;
+
 }
